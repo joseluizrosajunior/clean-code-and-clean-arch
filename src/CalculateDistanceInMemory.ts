68973bf93971110065e0119fe6e1ce087e64cdc1
jose.junior <jose.junior@betha.com.br>
2021-08-02 21:57:10 -0300

    PROJETO - PARTE 2

--- src/CalculateDistanceInMemory.ts
+++ src/CalculateDistanceInMemory.ts
@@ -1,7 +1,9 @@
-import CalculateDistance from "./CalculateDistanceInterface";
+import CalculateDistanceInterface from "./CalculateDistanceInterface";
 
-export default class CalculateDistanceInMemory implements CalculateDistance {
-    getDistance(): number {
+export default class CalculateDistanceInMemory implements CalculateDistanceInterface {
+    getDistance(cepOrigin: string, cepDestiny: string): number {
+        if (!cepOrigin) throw new Error("CEP origin is required");
+        if (!cepDestiny) throw new Error("CEP destiny is required");
         return 1000;
-    };
+    }
 }
